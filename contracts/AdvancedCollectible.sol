b9197814b938519480b748443adcfaeff5411e39
T-Damer <dpancov64@gmail.com>
2021-09-01 18:27:44 +0300

    some fixes

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.6;
 
-import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC721/ERC721.sol";
-import "smartcontractkit/chainlink-brownie-contracts@1.1.1/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
 
@@ -15,7 +15,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     mapping(bytes32 => address) public requestIdToSender;
     mapping(bytes32 => string) public requestIdToTokenURI;
     mapping(uint256 => Breed) public tokenIdToBreed;
-    mapping(bytes32 => uint256) public requestIdToTokenURI;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
     event requestCollectible(bytes32 indexed requestId);
 
     constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyHash) public 
@@ -32,7 +32,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         bytes32 requestId = requestRandomness(keyHash, fee);
         requestIdToSender[requestId] = msg.sender;
         requestIdToTokenURI[requestId] = tokenURI;
-        emit requestedCollectible(requestId);
+        emit requestCollectible(requestId);
     }
 
     function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
